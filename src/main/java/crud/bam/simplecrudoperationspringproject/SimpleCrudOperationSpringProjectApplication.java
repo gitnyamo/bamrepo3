7dae4f868295e85615d8dfa54d9b997bb763bb93
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-01-25 06:09:41 -0500

    html views with api controller simple learning UI project

---
+++ src/main/java/crud/bam/simplecrudoperationspringproject/SimpleCrudOperationSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package crud.bam.simplecrudoperationspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudOperationSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudOperationSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/crud/bam/simplecrudoperationspringproject/SimpleCrudOperationSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.bam.simplecrudoperationspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudOperationSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
