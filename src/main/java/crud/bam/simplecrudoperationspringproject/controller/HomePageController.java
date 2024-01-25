68a9ea6f4b1fe5b835308ca70bfa74e605832c86
margaretrnyamohanga <154075690+margaretrnyamohanga@users.noreply.github.com>
2024-01-25 07:16:25 -0500

    created the homepage controller and the linked logical index view. to improve if and when needed

---
+++ src/main/java/crud/bam/simplecrudoperationspringproject/controller/HomePageController.java
@@ -0,0 +1,13 @@
+package crud.bam.simplecrudoperationspringproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping("")
+    public String displayHomePage() {
+        return "index";//Note: We need to  create the index html file to return this logical view name
+    }
+}
